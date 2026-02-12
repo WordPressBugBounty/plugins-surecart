e0e65de1a8e609033478ac4abe4400b725a23ece
Sean Murphy <sean@iamseanmurphy.com>
2026-02-12 18:33:20 +0000

    3.20.0

--- surecart/dist/admin/settings/account.asset.php
+++ surecart/dist/admin/settings/account.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'regenerator-runtime', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '57d2d4929b0d955b5f96');
+<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'regenerator-runtime', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => 'cdf7386d52b75f448e98');
