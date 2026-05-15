03e12b4e67fb85c5d0d4733768ad45629af1f63c
Sean Murphy <sean@iamseanmurphy.com>
2026-05-15 18:46:02 +0000

    4.3.1

--- surecart/dist/blocks/cart.asset.php
+++ surecart/dist/blocks/cart.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-blockEditor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '6507fc882a7ec5f6b04a');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '6507fc882a7ec5f6b04a');
